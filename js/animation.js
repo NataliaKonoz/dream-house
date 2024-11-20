12823c970979e6f604f41a142faa62741c206e71
NataliaKonoz <Nataliia.Konoz@cs.khpi.edu.ua>
2024-11-20 12:21:40 +0200

    add background menu

--- js/animation.js
+++ js/animation.js
@@ -12,3 +12,32 @@ phoneIcon.addEventListener('mouseleave', () => {
   phoneIcon.classList.remove('hover');
   phoneIcon.classList.add('animate');
 });
+
+const bodyElement = document.body;
+const collapseElement = document.querySelector('.collapse');
+const menuButton = document.querySelector('.header__burger-icon');
+
+document.addEventListener('DOMContentLoaded', () => {
+  bodyElement.style.backgroundImage = 'url(../images/kitchen.png)';
+  bodyElement.style.setProperty('--after-bg-color', 'rgba(0, 0, 0, 0.45)');
+  let isClicking = false;
+
+  menuButton.addEventListener('click', () => {
+    if (isClicking) return;
+    isClicking = true;
+    setTimeout(() => {
+      isClicking = false;
+    }, 500);
+
+    if (bodyElement.style.backgroundImage.includes('kitchen.png')) {
+      bodyElement.style.backgroundImage =
+        'url(../images/background-navbar.png)';
+      bodyElement.style.setProperty('--after-bg-color', 'rgba(0, 0, 0, 0.6)');
+      bodyElement.style.backdropFilter = 'blur(6px)';
+    } else {
+      bodyElement.style.backgroundImage = 'url(../images/kitchen.png)';
+      bodyElement.style.setProperty('--after-bg-color', 'rgba(0, 0, 0, 0.45)');
+      bodyElement.style.backdropFilter = 'blur(0)';
+    }
+  });
+});
